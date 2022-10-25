36c13d238ec31f6f7e9f6fffc06a4a38988d86cd
Karl <local-part@domain>
2022-10-25 14:35:25 +0200

    [src/] strwtv.c updated: released My Mastermind.

--- src/strwtv.c
+++ src/strwtv.c
@@ -1,40 +1,23 @@
-/*// Copyright 2021 Karl V. P. Bertin
+/*//	Copyright 2021 Karl Vincent Pierre Bertin
 ////
-//// Redistribution and use in source and binary forms, with or without modifi-
-//// cation, are permitted provided that the following conditions are met:
+////	Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 ////
-////   1.  Redistributions of source code must retain the above copyright noti-
-////       ce, this list of conditions and the following disclaimer.
+////	1.	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 ////
-////   2.  Redistributions in binary form must reproduce the above copyright
-////       notice, this list of conditions and the following disclaimer in the
-////       documentation and/or other materials provided with the distribution.
+////	2.	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 ////
-////   3.  Neither the name of the copyright holder nor the names of its con-
-////       tributors may be used to endorse or promote products derived from
-////       this software without specific prior written permission.
+////	3.	Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 ////
-//// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-//// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-//// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-//// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRI-
-//// BUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLA-
-//// RY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
-//// OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSI-
-//// NESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-//// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHER-
-//// WISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-*/// OF THE POSSIBILITY OF SUCH DAMAGE.
+*///	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "../include/my_mastermind.h"
 
-int strwtv(char* secret, char* code) //int strwtv(char* pieces, char* secret, char* code)
+int strwtv(char* secret, char* code)
 {
     int n = 0;
     char pieces[8] = {'0', '1', '2', '3', '4', '5', '6', '7'};
-    int m = sizeof(pieces) / sizeof(pieces[0]); //int m = arrlen(pieces); //int m = strlen(pieces);
-    //char* buf1 = malloc( n * sizeof(char) );
-    int buffer1[8] = {0, 0, 0, 0, 0, 0, 0, 0}; //char buf1[m]
+    int m = sizeof(pieces) / sizeof(pieces[ 0 ]);
+    int buffer1[8] = {0, 0, 0, 0, 0, 0, 0, 0};
     int buffer2[8] = {0, 0, 0, 0, 0, 0, 0, 0};
     int l_1 = strlen(secret);
     int l_2 = strlen(code);
@@ -44,45 +27,43 @@ int strwtv(char* secret, char* code) //int strwtv(char* pieces, char* secret, ch
 
 	if ( l_2 != l_1 )
 		return -1;
-	// Part 0.
+
 	k = 0;
 	while ( k < m )
 	{
 		i = 0;
 		while ( i < l_1 )
 		{
-			if (secret[i] == pieces[k])
-				buffer1[k] += 1;
+			if (secret[ i ] == pieces[ k ])
+				buffer1[ k ] += 1;
 			i++;
 		}
 		j = 0;
 		while ( j < l_2 )
 		{
-			if (code[j] == pieces[k])
-				buffer2[k] += 1;
+			if (code[ j ] == pieces[ k ])
+				buffer2[ k ] += 1;
 			j++;
 		}
 		k++;
 	}
-	// Part 1: Count the amounts of pieces in both the secret and the code.
+
 	k = 0;
 	while ( k < m )
 	{
-		if ( buffer2[k] >= buffer1[k] && buffer1[k] >= 1 )
-			n += buffer1[k];
-		else if ( buffer2[k] <= buffer1[k] && buffer2[k] >= 1 )
-			n += buffer2[k];
+		if ( buffer2[ k ] >= buffer1[ k ] && buffer1[ k ] >= 1 )
+			n += buffer1[ k ];
+		else if ( buffer2[ k ] <= buffer1[ k ] && buffer2[ k ] >= 1 )
+			n += buffer2[ k ];
 		k++;
 	}
-	// Part 2: Count shared pieces.
+
 	return n;
 }
 
-/*	strwtv.c
-	Karl's STRwhatever
-
-	Karl Bertin `bertin_k`
-	bertin_k@qwasar.io
-	https://upskill.us.qwasar.io/users/bertin_k
-
-*///	
+/*//	strwtv.c
+////	STRWhatever
+////
+////	Karl Bertin `bertin_k`
+////	bertin_k@qwasar.io
+*///	https://upskill.us.qwasar.io/users/bertin_k
