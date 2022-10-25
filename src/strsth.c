74acccbd47e54b856bae33362783029684072bdc
Karl <local-part@domain>
2022-10-25 14:35:12 +0200

    [src/] strsth.c updated: released My Mastermind.

--- src/strsth.c
+++ src/strsth.c
@@ -1,30 +1,14 @@
-/*// Copyright 2021 Karl V. P. Bertin
+/*//	Copyright 2021 Karl Vincent Pierre Bertin
 ////
-//// Redistribution and use in source and binary forms, with or without modifi-
-//// cation, are permitted provided that the following conditions are met:
+////	Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 ////
-////   1.  Redistributions of source code must retain the above copyright noti-
-////       ce, this list of conditions and the following disclaimer.
+////	1.	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 ////
-////   2.  Redistributions in binary form must reproduce the above copyright
-////       notice, this list of conditions and the following disclaimer in the
-////       documentation and/or other materials provided with the distribution.
+////	2.	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 ////
-////   3.  Neither the name of the copyright holder nor the names of its con-
-////       tributors may be used to endorse or promote products derived from
-////       this software without specific prior written permission.
+////	3.	Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 ////
-//// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-//// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-//// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-//// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRI-
-//// BUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLA-
-//// RY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
-//// OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSI-
-//// NESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-//// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHER-
-//// WISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-*/// OF THE POSSIBILITY OF SUCH DAMAGE.
+*///	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "../include/my_mastermind.h"
 
@@ -37,18 +21,16 @@ int strsth(char* s_1, char* s_2) // receives the string of the secret code and t
 		return 0;
 	while ( j < strlen(s_1) )
 	{
-		if (s_2[j] == s_1[j])
+		if (s_2[ j ] == s_1[ j ])
 			i += 1;
 		j++;
 	}
 	return i;
 }
 
-/*	strsth.c
-	Karl's STRsomething
-
-	Karl Bertin `bertin_k`
-	bertin_k@qwasar.io
-	https://upskill.us.qwasar.io/users/bertin_k
-
-*///	
+/*//	strsth.c
+////	STRSomething
+////
+////	Karl Bertin `bertin_k`
+////	bertin_k@qwasar.io
+*///	https://upskill.us.qwasar.io/users/bertin_k
